e120cde59dfa3d1c78a40f84b502e31cb891c20f
AHuang170 <32000451+AHuang170@users.noreply.github.com>
2019-09-16 15:58:38 -0700

    Improve readability and initial refactoring

--- module02/host_network_setup.sh
+++ module02/host_network_setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -x
+echo "Setting up VM NAT network"
 vboxmanage () { VboxManage.exe "$@"; }
 vboxmanage natnetwork add --netname net_4640 --network "192.168.250.0/24" --enable --dhcp off --ipv6 off
 vboxmanage natnetwork modify --netname net_4640 --port-forward-4 "ssh:tcp:[]:50022:[192.168.250.10]:22"
 vboxmanage natnetwork modify --netname net_4640 --port-forward-4 "http:tcp:[]:50080:[192.168.250.10]:80"
 vboxmanage natnetwork modify --netname net_4640 --port-forward-4 "https:tcp:[]:50443:[192.168.250.10]:443"
 vboxmanage natnetwork start --netname net_4640
+echo "DONE!"
